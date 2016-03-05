e7d4f11b51fc9cb72fecfffe6977af343871027f
Jewl Johnson <jewljohnson43@gmail.com>
2016-03-04 23:43:17 -0500

    Status:Modified Main and the isLessThanTwenty method so that most of the computing is done outside of main.
    Recommendations: Add scanner object so that code can be ran from an IDE
    or as a stand alone program. 
    
    Next Function to implement: extending the program to handle numbers less
    100.

--- NumberModules/src/Module1.java
+++ NumberModules/src/Module1.java
@@ -1,4 +1,6 @@
-public class Module1 {
+public class Module1 
+{
+	private static int j;
 	
 	public static void main(String[] args) 
 	{
@@ -10,87 +12,89 @@ public class Module1 {
 		 * extended later to include 999,999,999,999.
 		 */
 		
-		int j = Integer.parseInt(args[0]);
+		j = Integer.parseInt(args[0]);	
 		
+		if( j < 20 ) // If the inputted integer is less than twenty a simple switch statement will be used
+		{
+			isLessThanTwenty(j);
+		}
+	}
+	private static void isLessThanTwenty(int i)
+	{
+		//Pre: Receive input from user that is less 20
+
+		switch( i )
+		{
+			case 1: System.out.println("one");
+			break;
+			
+			case 2: System.out.println("two");
+			break;
+			
+			case 3: System.out.println("three");
+			break;
+			
+			case 4: System.out.println("four");
+			break;
+			
+			case 5: System.out.println("five");
+			break;
+			
+			case 6: System.out.println("six");
+			break;
+			
+			case 7: System.out.println("seven");
+			break;
+			
+			case 8: System.out.println("eight");
+			break;
+			
+			case 9: System.out.println("nine");
+			break;
+			
+			case 10: System.out.println("ten");
+			break;
+			
+			case 11: System.out.println("eleven");
+			break;
+			
+			case 12: System.out.println("twelve");
+			break;
+			
+			case 13: System.out.println("thirteen");
+			break;
+			
+			case 14: System.out.println("fourteen");
+			break;
+			
+			case 15: System.out.println("fifteen");
+			break;
+			
+			case 16: System.out.println("sixteen");
+			break;
+			
+			case 17: System.out.println("seventeen");
+			break;
+			
+			case 18: System.out.println("eighteen");
+			break;
+			
+			case 19: System.out.println("nineteen");
+			break;
+			
+			default:
+					System.out.println("The number you inputed must be greater than 20");
+			break;
+		}
+		
+		//Post: Prints the name of the integer the client typed.
+		
+		//---------------WARNING-------------------------\\
 		//Extending this method to include numbers would be entirely to tedious
 		/*
-		 * Instead I will be writing this program to identify whole numbers via brute force implementation.
-		 */
-		if( isLessThanTwenty( j ) ) // If the inputted integer is less than twenty a simple switch statement will be used
-		{
-			System.out.println( j );
-			switch( j )
-			{
-				case 1: System.out.println("one");
-				break;
-				
-				case 2: System.out.println("two");
-				break;
-				
-				case 3: System.out.println("three");
-				break;
-				
-				case 4: System.out.println("four");
-				break;
-				
-				case 5: System.out.println("five");
-				break;
-				
-				case 6: System.out.println("six");
-				break;
-				
-				case 7: System.out.println("seven");
-				break;
-				
-				case 8: System.out.println("eight");
-				break;
-				
-				case 9: System.out.println("nine");
-				break;
-				
-				case 10: System.out.println("ten");
-				break;
-				
-				case 11: System.out.println("eleven");
-				break;
-				
-				case 12: System.out.println("twelve");
-				break;
-				
-				case 13: System.out.println("thirteen");
-				break;
-				
-				case 14: System.out.println("fourteen");
-				break;
-				
-				case 15: System.out.println("fifteen");
-				break;
-				
-				case 16: System.out.println("sixteen");
-				break;
-				
-				case 17: System.out.println("seventeen");
-				break;
-				
-				case 18: System.out.println("eighteen");
-				break;
-				
-				case 19: System.out.println("nineteen");
-				break;
-				
-				default:
-						System.out.println("The number you inputed must be greater than 20");
-				break;
-			}
-		}
-
-	}
-	
-	private static boolean isLessThanTwenty(int i)
-	{
-		
-		return i < 20;
-		
+		 * Instead I will be writing this program to identify whole numbers via brute 
+		 * force implementation.
+		 */		
 	}
 
 }
